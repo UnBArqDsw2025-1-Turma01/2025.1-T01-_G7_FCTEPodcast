bcd0f29b602d165a81867585904b418933bd143f
IderlanJ <iderlanblack9@gmail.com>
2025-06-15 17:54:36 -0300

    feat: Criando primeira interface da tela de gerenciar monitor

--- fctepodcast-frontend/src/pages/podcasts/GerenciarMonitores.tsx
+++ fctepodcast-frontend/src/pages/podcasts/GerenciarMonitores.tsx
@@ -1,5 +1,41 @@
+import { Button, Divider, useDisclosure } from "@heroui/react";
+import { motion } from "framer-motion";
+
 const GerenciarMonitores = () => {
-  return <div>GerenciarMonitores</div>;
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  
+  return (
+    <div className="w-full h-full flex flex-col gap-4">
+      <motion.div
+        initial={{ opacity: 0, y: -10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4 }}
+        className="flex items-center
+        justify-between"
+      >
+       <h1 className="font-bold
+       text-2xl">Monitores</h1>
+       <Button onPress={onOpen}
+       color="primary">
+         Adicionar Monitor
+       </Button>  
+      </motion.div>
+
+      <Divider />
+
+      <motion.div
+        initial={{ opacity: 0, y: -5}}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.4, delay: 0.1 }}
+      >
+        <p className="text-gray-500">
+          Aqui você pode gerenciar seus monitores, e adicionar novos monitores aos Podcasts.
+        </p>
+      </motion.div>
+
+      
+    </div>
+  );
 };
 
 export default GerenciarMonitores;
