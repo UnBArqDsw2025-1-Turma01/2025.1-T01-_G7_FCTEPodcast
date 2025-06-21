9aa125fb49dd9cdecc78d14e1379d4bcdc16a923
cwtshh <gucosta1719@gmail.com>
2025-06-20 22:33:55 -0300

    feat: adicionando "thread" ao comentario

--- fctepodcast-frontend/src/components/resposta-card/RespostaCard.tsx
+++ fctepodcast-frontend/src/components/resposta-card/RespostaCard.tsx
@@ -30,7 +30,7 @@ const RespostaCard = ({
   resposta: {
     conteudo: string;
     createdAt: string;
-    tag: Tag;
+    tag: string;
     usuario: {
       _id: string;
       nome: string;
@@ -46,36 +46,44 @@ const RespostaCard = ({
       animate={{ opacity: 1, y: 0 }}
       transition={{ duration: 0.3 }}
     >
-      <div className="bg-primary-200 p-2 w-[96%] flex flex-col gap-2 rounded-xl">
-        <div className="flex gap-2 items-center mb-2">
-          <Avatar size="lg" />
-          <div className="flex flex-col w-full">
-            <div className="flex items-center gap-3">
-              <div>
-                <p className="font-bold">{resposta.usuario.nome}</p>
-                <p className="text-sm italic">{resposta.usuario.email}</p>
-              </div>
-              <Chip
-                startContent={iconMap[resposta.tag as Tag] || null}
-                variant="shadow"
-                color={colorMap[resposta.tag as Tag] || "default"}
-              >
-                {resposta.tag.charAt(0).toUpperCase() + resposta.tag.slice(1)}
-              </Chip>
-            </div>
-            <p className="text-sm text-gray-400">
-              {resposta.createdAt
-                ? `${new Date(resposta.createdAt).toLocaleDateString(
-                    "pt-BR"
-                  )} às ${new Date(resposta.createdAt).toLocaleTimeString(
-                    "pt-BR"
-                  )}`
-                : "Data desconhecida"}
-            </p>
-          </div>
+      <div className="relative w-[100%] flex">
+        {/* Linha da thread à esquerda */}
+        <div className="w-4 flex justify-center">
+          <div className="w-[3px] bg-gray-50 rounded-full h-full" />
         </div>
-        <p className="font-bold">Respondeu: </p>
-        {resposta.conteudo}
+
+        {/* Conteúdo da resposta */}
+        <div className="bg-primary-200 p-2 flex-1 flex flex-col gap-2 rounded-xl ml-2">
+          <div className="flex gap-2 items-center mb-2">
+            <Avatar size="lg" />
+            <div className="flex flex-col w-full">
+              <div className="flex items-center gap-3">
+                <div>
+                  <p className="font-bold">{resposta.usuario.nome}</p>
+                  <p className="text-sm italic">{resposta.usuario.email}</p>
+                </div>
+                <Chip
+                  startContent={iconMap[resposta.tag as Tag] || null}
+                  variant="shadow"
+                  color={colorMap[resposta.tag as Tag] || "default"}
+                >
+                  {resposta.tag.charAt(0).toUpperCase() + resposta.tag.slice(1)}
+                </Chip>
+              </div>
+              <p className="text-sm text-gray-400">
+                {resposta.createdAt
+                  ? `${new Date(resposta.createdAt).toLocaleDateString(
+                      "pt-BR"
+                    )} às ${new Date(resposta.createdAt).toLocaleTimeString(
+                      "pt-BR"
+                    )}`
+                  : "Data desconhecida"}
+              </p>
+            </div>
+          </div>
+          <p className="font-bold">Respondeu: </p>
+          {resposta.conteudo}
+        </div>
       </div>
     </motion.div>
   );
